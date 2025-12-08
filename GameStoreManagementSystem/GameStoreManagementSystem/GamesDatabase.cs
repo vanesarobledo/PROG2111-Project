2a8872c4c1154755ef1fa4236aea113d14c8e35d
Vanesa <77148922+vanesarobledo@users.noreply.github.com>
2025-12-08 11:36:50 -0500

    Added MySql NuGet package, App.config file, GamesDatabase class to connect to database

---
+++ GameStoreManagementSystem/GameStoreManagementSystem/GamesDatabase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace GameStoreManagementSystem
+{
+    internal class GamesDatabase
+    {
+        /// <summary>
+        /// Password for connecting to MySql database
+        /// </summary>
+        private static string password = "";
+
+        /// <summary>
+        /// Connection string to connect to database
+        /// </summary>
+        private static string connectionString = "Server=localhost;Port=3306;Uid=root;Pwd=" + password + ";Database=games;";
+
+        /// <summary>
+        /// DataAdapter to connect to MySql database
+        /// </summary>
+        private MySqlDataAdapter adapter;
+
+        // Constructor
+        public GamesDatabase()
+        {
+            if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["password"]))
+            {
+                password = ConfigurationManager.AppSettings["password"];
+            }
+            adapter = new MySqlDataAdapter("SELECT * FROM Game", connectionString);
+        }
+
+        // Functions
+
+        /// <summary>
+        /// Loads the data of a table from the games database to a dataset
+        /// </summary>
+        /// <param name="table">Name of the table to load</param>
+        internal DataSet LoadData(string table)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM " + table;
+                    adapter = new MySqlDataAdapter(query, conn);
+                    ds = new DataSet();
+                    adapter.Fill(ds, table);
+                    //dataGrid.ItemsSource = ds.Tables[table].DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error connecting to database: " + e.ToString());
+            }
+            return ds;
+        }
+
+        /// <summary>
+        /// Updates the data from a dataset to a table in the games database
+        /// </summary>
+        /// <param name="ds">Dataset with updated data</param>
+        /// <param name="table">Name of table to update</param>
+        internal void SaveData(DataSet ds, string table)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM " + table;
+                    adapter = new MySqlDataAdapter(query, conn);
+                    MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+                    adapter.Update(ds, table);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error connecting to database to update data: " + e.ToString());
+            }
+        }
+
+    }
+}
--- GameStoreManagementSystem/GameStoreManagementSystem/MainWindow.xaml.cs
+++ GameStoreManagementSystem/GameStoreManagementSystem/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Text;
+﻿/*
+* FILE          : MainWindow.xaml.cs
+* PROJECT       : PROG2111 Project
+* PROGRAMMER    : Eumee Garcia, Vanesa Robledo
+* FIRST VERSION : 2025-12-08
+* DESCRIPTION   : This is an application to manage the database for managing the data required for a game store.
+*/
+
+using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +17,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 
 namespace GameStoreManagementSystem
@@ -17,10 +28,29 @@ namespace GameStoreManagementSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Main Window
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialize games database
+            GamesDatabase gamesDatabase = new GamesDatabase();
         }
+
+        // Functions
+
+        /// <summary>
+        /// Loads a dataset into the DataGrid
+        /// </summary>
+        /// <param name="ds">Dataset to load</param>
+        internal void LoadDataGrid(DataSet ds)
+        {
+            if (ds != null)
+            {
+                //TODO: Load dataset into the DataGrid
+            }
+        }
+
         private void OpenGames_Click(object sender, RoutedEventArgs e)
         {
             //GameView window = new GameView();
