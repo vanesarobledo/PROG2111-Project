0ed490dd556102669a8e5d0170c525f3bff11b9d
Vanesa <77148922+vanesarobledo@users.noreply.github.com>
2025-12-09 16:30:30 -0500

    Added auto-populating games from games dataset

--- GameStoreManagementSystem/GameStoreManagementSystem/Forms/ManageInventoryForm/AddInventoryForm.xaml.cs
+++ GameStoreManagementSystem/GameStoreManagementSystem/Forms/ManageInventoryForm/AddInventoryForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,52 @@ namespace GameStoreManagementSystem.Forms.ManageInventoryForm
         public AddInventoryForm()
         {
             InitializeComponent();
+
+            // Auto-populate foreign key fields
+            LoadForeignKeys();
         }
 
+        internal void LoadForeignKeys()
+        {
+            // Select games database
+            GamesDatabase db = ((MainWindow)Application.Current.MainWindow).gamesDatabase;
+
+            // Auto-populate games
+            GameSelect.SelectedValuePath = "Key";
+            GameSelect.DisplayMemberPath = "Value";
+
+            foreach (DataRow game in db.Game.Rows)
+            {
+                int gameID = (int)game["game_id"];
+                string gameDisplay = game["game_id"].ToString() + ": " + game["title"];
+                GameSelect.Items.Add(new KeyValuePair<int, string>(gameID, gameDisplay));
+            }
+        }
+
+        /*
+        * With help from:
+        * TITLE : “WPF TextBox Only Accepts Numbers”
+        * AUTHOR : Mike
+        * DATE : 2020-05-16
+        * VERSION : 1.0
+        * AVAILABIILTY : https://begincodingnow.com/wpf-textbox-only-accepts-numbers/
+        */
+        /// <summary>
+        /// Only allow numeric input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void InputQuantity_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Validation.IsNum(e.Text);
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
         {
 
         }
