0f481fd3ba42645b00c8de06f2b2f9c2dce25f09
ThomConn <Cthompson8456@conestogac.on.ca>
2025-12-10 00:28:16 -0500

    Adds done
    Add Store and add employee are done

---
+++ GameStoreManagementSystem/GameStoreManagementSystem/Forms/ManageEmployeeForm/AddEmployeeForm.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameStoreManagementSystem.Forms.ManageEmployeeForm
+{
+    /// <summary>
+    /// Interaction logic for AddEmployeeForm.xaml
+    /// </summary>
+    public partial class AddEmployeeForm : Window
+    {
+        internal GamesDatabase db = ((MainWindow)Application.Current.MainWindow).gamesDatabase;
+        public AddEmployeeForm()
+        {
+            InitializeComponent();
+            LoadForeignKeys();
+        }
+
+        internal void LoadForeignKeys()
+        {
+            foreach (DataRow store in db.Store.Rows)
+            {
+                int storeID = (int)store["store_id"];
+                string storeDisplay = store["store_id"].ToString() + ": " + store["location"];
+                storeIDBox.Items.Add(new KeyValuePair<int, string>(storeID, storeDisplay));
+            }
+        }
+
+        internal bool emptyCheck()
+        {
+            bool retValue = false;
+            
+            if (firstNameBox != null && lastNameBox != null && dobBox != null && emailBox !=null
+                && usernameBox != null && passwordBox != null && storeIDBox.Text != null)
+            {
+                retValue = true;
+            }
+            return retValue;
+        }
+        private void AddEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow newRow = db.Employee.NewRow();
+            if (emptyCheck())
+            {
+                newRow["first_name"] = firstNameBox.Text;
+                newRow["last_name"] = lastNameBox.Text;
+                newRow["date_of_birth"] = dobBox.Text;
+                newRow["email"] = emailBox.Text;
+                newRow["username"] = usernameBox.Text;
+                newRow["password"] = passwordBox.Text;
+                newRow["store_id"] = storeIDBox.Text;
+                db.Employee.Rows.Add(newRow);
+            }
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ GameStoreManagementSystem/GameStoreManagementSystem/Forms/ManageStoreForm/AddStore.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameStoreManagementSystem.Forms.ManageStoreForm
+{
+    /// <summary>
+    /// Interaction logic for AddStore.xaml
+    /// </summary>
+    public partial class AddStore : Window
+    {
+
+        internal GamesDatabase db = ((MainWindow)Application.Current.MainWindow).gamesDatabase;
+        public AddStore()
+        {
+            InitializeComponent();
+        }
+
+        private void AddStoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow newRow = db.Store.NewRow();
+
+
+            if (StoreNameInput != null)
+            { 
+                newRow["location"] = StoreNameInput.Text;
+                db.Store.Rows.Add(newRow);
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
